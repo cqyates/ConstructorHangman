982674efd4319513d71303a47354b881d6e73298
Corey <cqyates@gmail.com>
2019-07-14 16:18:14 -0400

    finished letter constructor

---
+++ letter.js
@@ -0,0 +1,20 @@
+let letter = function (letter) {
+    this.letter = letter;
+    this.guessed = false;
+
+
+    this.displayletter = function () {
+        if (this.guessed) {
+            return this.letter + " ";
+        }
+        else {
+            return "_";
+        }
+    }
+
+    this.checkGuess = function (guess) {
+        if (guess === this.letter) {
+            this.guessed = true;
+        }
+    }
+}
